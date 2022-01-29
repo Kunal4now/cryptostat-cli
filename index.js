84feea1283d75c7714a6c2d1dafab7c44b4d7654
Kunal <kunal.rohitas29@gmail.com>
2022-01-29 14:52:52 +0530

    Add table

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import cheerio from 'cheerio'
 import axios from 'axios'
 import inquirer from 'inquirer'
 import chalk from 'chalk'
+import Table from 'cli-table3'
 
 const url = 'https://coinmarketcap.com/'
 
 const getPrices = async (cryptoName) => {
     try {
+        const currency = cryptoName.cryptoName 
         const {data} = await axios.get(url, {timeout: 500000});
         const $ = await cheerio.load(data)
         const items = "#__next div div.main-content div.sc-57oli2-0.comDeo.cmc-body-wrapper div div div.h7vnx2-1.bFzXgL table tbody tr"
@@ -16,15 +18,16 @@ const getPrices = async (cryptoName) => {
             'rank',
             'name',
             'price',
-            '24h',
-            '7d',
+            'allDay',
+            'weekLong',
             'marketCap',
             'volume',
             'circulatingSupply'
         ]
 
         let coinArray = []
-
+        let foundVal = {};
+        foundVal.success = false
         $(items).each((parentIdx, parentElm) => {
             let keyIdx = 0;
             let coinObj = {}
@@ -45,7 +48,13 @@ const getPrices = async (cryptoName) => {
                 coinArray.push(coinObj)
             }
         })
-        return coinArray
+        coinArray.forEach((coin) => {
+            if (coin.name.toLowerCase() === currency) {
+                foundVal = coin;
+                foundVal.success = true
+            }
+        })
+        return foundVal;
     } catch(err) {
         console.log(err);
     }
@@ -56,9 +65,17 @@ inquirer.prompt([
         type: 'input',
         name: 'cryptoName',
         message: chalk.bgCyan(chalk.white('Enter the crypto name')),
-        // default: 'bitcoin'
     }
 ]).then(async (answer)  => {
     const val = await getPrices(answer);
-    console.log(val);
+    if (!val.success) {
+        console.log("Invalid currency");
+        return;
+    }
+    const {rank,name, price , allDay, weekLong, marketCap, volume, circulatingSupply} = val
+    var table = new Table({
+        head: [chalk.cyan("rank"), chalk.cyan("name"), chalk.cyan("price"), chalk.cyan("24h"), chalk.cyan("7d"), chalk.cyan("marketCap"), chalk.cyan("volume"), chalk.cyan("circulatingSupply")]
+    });
+    table.push([rank, name, price, allDay, weekLong, marketCap, volume, circulatingSupply])
+    console.log(table.toString())
 })
