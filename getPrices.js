c5e3a8cb152855007981103e7f5b776a7ccc38c6
Kunal <kunal.rohitas29@gmail.com>
2022-01-29 16:11:47 +0530

    Add getPrices

---
+++ getPrices.js
@@ -0,0 +1,58 @@
+import cheerio from 'cheerio'
+import axios from 'axios'
+import configValues from './config.js'
+
+const getPrices = async (cryptoName) => {
+    try {
+        const {data} = await axios.get(configValues.url, {timeout: 500000});
+        const $ = await cheerio.load(data)
+        
+        let coinArray = []
+        let foundVal = {};
+        foundVal.success = false
+        let cryptoNameMap = new Map();
+        let cryptoNameValue;
+
+        $(configValues.selector).each((parentIdx, parentElm) => {
+            let keyIdx = 0;
+            let coinObj = {}
+            if (parentIdx <= 9) {
+                $(parentElm).children().each((childrenIdx, childrenElm) => {
+                    let tdVal = $(childrenElm).text();
+                    if (childrenIdx === 6) {
+                        tdVal = $('p:first-child span:nth-child(2)', $(childrenElm).html()).text()
+                    }
+                    if (keyIdx === 1) {
+                        cryptoNameValue = $('p:first-child', $(childrenElm).html()).text()
+                        tdVal = $('p:nth-child(2)', $(childrenElm).html()).text().toLowerCase().toString()
+                        cryptoNameMap.set(tdVal, cryptoNameValue)
+                    }
+                    if (keyIdx === 6) {
+                        tdVal = $('p:first-child', $(childrenElm).html()).text()
+                    }
+                    if (tdVal) {
+                        if (keyIdx == 1) {
+                            coinObj[configValues.keys[keyIdx]] = cryptoNameValue
+                        } else {
+                            coinObj[configValues.keys[keyIdx]] = tdVal
+                        }
+                        keyIdx++;
+                    }
+                })
+                coinArray.push(coinObj)
+            }
+        })
+        const currency = cryptoNameMap.get(cryptoName.cryptoName.toString()) 
+        coinArray.forEach((coin) => {
+            if (coin.name === currency) {
+                foundVal = coin;
+                foundVal.success = true
+            }
+        })
+        return foundVal;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+export default getPrices
