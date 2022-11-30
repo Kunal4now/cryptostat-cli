81d30c028c1b3513a4f182fb86cac963621afd85
Kunal <kunal.rohitas29@gmail.com>
2022-11-30 18:51:00 +0530

    Update selector

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 let configValues = {
-    selector: "#__next div div.main-content div.sc-1a736df3-0.PimrZ.cmc-body-wrapper div div div.sc-853bfcae-1.eibzVK table tbody tr",
+    // selector: "#__next div div.main-content div.sc-1a736df3-0.PimrZ.cmc-body-wrapper div div div.sc-853bfcae-1.eibzVK table tbody tr",
+    selector: "#__next  div  div.main-content  div.sc-1a736df3-0.PimrZ.cmc-body-wrapper  div  div:nth-child(1)  div.sc-f7a61dda-2.efhsPu  table  tbody tr",
     url:'https://coinmarketcap.com/',
     keys: [
         'rank',
